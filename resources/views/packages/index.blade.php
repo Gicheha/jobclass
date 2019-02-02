646b9c4425cf5c873a4518272bc62184d615400b
Gicheha <geoffreygicheha@gmail.com>
2019-02-02 23:23:40 +0300

    Finished Packages Page

--- resources/views/packages/index.blade.php
+++ resources/views/packages/index.blade.php
@@ -7,7 +7,7 @@
 
 @section('content')
     <!--Nav Tabs-->
-    <div class="pagination-centered">
+    <div class="text-center">
         <ul class="nav nav-tabs" role="tablist">
             <li class="active"><a href="#home" role="tab" data-toggle="tab">Free Packages</a></li>
             <li><a href="#paid" role="tab" data-toggle="tab">Paid Packages</a></li>
@@ -31,76 +31,80 @@
                     @include('common.spacer')
                     <div class="row">
                         <div class="column-33 col-md-2 col-md-offset-5">
-                            <button>Get Free Demo</button>
+                            <button class="btn btn-large" style="background:#9C27B0;">Get Free Demo</button>
                         </div>
                     </div>
                     @include('common.spacer')
                     <div class="row">
-                        <div class="col-md-2 col-md-offset-5">
+                        <div>
                         @foreach($packages as $key => $package)
-                            <div class="card mx-auto" style="width: 18rem;">
-                                <div class="card-body">
-                                    <h5 class="card-title">{{$package->name}}</h5>
-                                    <h6 class="card-subtitle mb-2 text-muted">{{$package->price}}</h6>
-                                    <p class="card-text">{{$package->description}}</p>
-                                    <a href="#" class="card-link">$package->duration</a>
-                                    <a href="#" class="card-link">Another link</a>
+                                <div class="border-black" style="border-style:solid; border-radius:2em;border-width:medium;border-color:#03A9F4">    
+                                    <div class="card text-primary border-black" >
+                                        <div class="card-body">
+                                            <h5 class="card-title logo text-center">{{$package->name}}</h5>
+                                            <h6 class="card-subtitle mb-2 text-center" style="font-size:1.75em"> $ {{$package->price}}</h6>
+                                            <p class="card-text text-center">{{$package->description}}</p>
+                                            <p class="card-text text-center"> Valid for {{$package->duration}} Days </p>
+                                        </div>
+                                    </div>    
                                 </div>
-                            </div>
+                            @include('common.spacer')
                         @endforeach
-                    </div>
+                        </div>
                     </div>    
                 </div>    
             </div>    
         </div>
         <div class="tab-pane" id="paid">
-            <div>
-                <h2>Find relevant candidates with the World's most advanced keyword-search engine*</h2>
-                <ul>
-                    <li>Perform Keyword Searches with 11 filters to <b>get accurate results</b></li>
-                    <li>Sort and Filter candidates based on their skills, relevance and qualifications</li>
-                    <li><b>Download Candidate information</b> directly from the search page with a single click</li>
-                    <li>Simplified way to <b>convert search into job postings</b> and receive applications</li>
-                </ul>
-                
+            <div class="column-66">
+                <h2 class="text-center">Get the most relevant candidates auto-matched to your job openings</h2>
+                    <ul class="text-center">
+                        <li>Perform Keyword Searches with 11 filters to <b>get accurate results</b></li>
+                        <li>Sort and Filter candidates based on their skills, relevance and qualifications</li>
+                        <li><b>Download Candidate information</b> directly from the search page with a single click.</li>
+                        <li>implified way to <b>convert search into job postings</b> and receive applicationsr</li>
+                    </ul>
             </div>
+            @include('common.spacer')
+            <div class="row">
+                <div class="column-33 col-md-2 col-md-offset-5">
+                    <button class="btn btn-large" style="background:#9C27B0;">Get Free Demo</button>
+                </div>
+            </div>
+            @include('common.spacer')
             <div>
                 @foreach($packages as $key => $package)
-                    <div class="card mx-auto" style="width: 18rem;">
-                        <div class="card-body">
-                            <h5 class="card-title">{{$package->name}}</h5>
-                            <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" class="card-link">Card link</a>
-                            <a href="#" class="card-link">Another link</a>
-                        </div>
+                    <div class="border-black" style="border-style:solid; border-radius:2em;border-width:medium;border-color:#03A9F4">
+                        <div class="card text-primary border-black">
+                            <div class="card-body">
+                                <h5 class="card-title logo text-center">{{$package->name}}</h5>
+                                <h6 class="card-subtitle mb-2 text-center">$ {{$package->price}}</h6>
+                                <p class="card-text text-center">{{$package->description}}</p>
+                                <p class="card-text text-center">Valid for {{$package->duration}} Days</p>
+                            </div>
+                        </div>    
                     </div>
+                @include('common.spacer')
                 @endforeach
-            </div>
-            <div>
-                <button>Get Free Demo</button>
             </div>
         </div>
         <div class="tab-pane" id="package">
+            @include('common.spacer')
             <form method="post" action="" role="form" class="pagination-centered">
-                <div class="form-group text-center">
-                    <div class="form-group mb-2"><input type="text" name="Name" required placeholder="Name" /></div>
-                    <div class="form-group mb-2"><input type="email" name="Email" required placeholder="Email"/></div>
-                    <div class="form-group mb-2"><input type="text" name="Mobile" required placeholder="Mobile No"/></div>
-                    <select name="interest"  >
-                    <option></option>
-                    <option></option>
-                    <option></option>
-                    <option></option>
-                </select>
-                    <select name="interest">
-                    <option></option>
-                    <option></option>
-                    <option></option>
-                    <option></option>
-                </select>
-                    <input type="hidden" name="_token" id="crsf-token" value="{{Session::token()}}"/>
-                    <div class="form-group mb-2"><input type="submit" class="btn btn-primary" value="submit"/></div>
+                <div class="row">    
+                    <div class="form-group text-center">
+                        <div class="form-group mb-2"><input type="text" class="form-control" name="Name" required placeholder="Name" /></div>
+                        <div class="form-group mb-2"><input type="email" class="form-control" name="Email" required placeholder="Email"/></div>
+                        <div class="form-group mb-2"><input type="text" class="form-control input-normal" name="Mobile" required placeholder="Mobile No"/></div>
+                        <div class="form-group mb-2"><select name="interest" class="form-control">
+                            @foreach($packages as $key => $package)
+                                <option value="{{$package->name}}">{{$package->name}}</option>
+                            @endforeach
+                        </select>
+                        </div>    
+                        <input type="hidden" name="_token" id="crsf-token" value="{{Session::token()}}"/>
+                        <div class="form-group mb-2"><input type="submit" class="btn btn-primary" value="submit"/></div>
+                    </div>
                 </div>    
             </form>
         </div>
